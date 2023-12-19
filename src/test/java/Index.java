a7421f80976a19961437b9887fdef2c6ab352f9a
YuriiVolodarskyi <alimych65@gmail.com>
2023-12-19 13:41:20 +0200

    add test to index

--- src/test/java/Index.java
+++ src/test/java/Index.java
@@ -1,6 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.util.List;
 
 public class Index {
 
@@ -12,4 +17,19 @@ public class Index {
         wd.get("file:///C:/Users/User/Downloads/21.index.html");
     }
 
+    @Test
+    public void cssLocators() {
+        //by tag name
+        WebElement element = wd.findElement(By.tagName("button"));
+        List<WebElement> list = wd.findElements(By.tagName("a"));
+
+
+    }
+
+    @AfterClass
+    public void stop(){
+        wd.close();
+    }
+
 }
+
